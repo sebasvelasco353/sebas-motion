e30596b4e2988ba0392a3dc9a430eade47999f17
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2021-02-19 22:45:28 -0600

    agregue idea de como resolver el problema.

--- components/projectPreview.vue
+++ components/projectPreview.vue
@@ -1,5 +1,5 @@
 <template>
-<nuxt-link :to="`projects/${element.slug}`" exact>
+<nuxt-link :to="`projects/${element.slug}`" :style="gridPosition" exact>
   <div class="projPreview__container" :class="isLarge">
     <img class="bg" :src="require(`@/assets/images/${element.image}`)" :alt="element.title" />
     <p>{{ element.title }}</p>
@@ -20,9 +20,25 @@ export default {
     },
   },
   data({ index }) {
-    console.log(index);
+    /*
+      TODO: Si voy a mostrar siempre los ultimos 5 proyectos lo que puedo hacer es definir las 5 areas
+      con nombres 1...5 y llamar en el index a los ultimos 5 proyectos y a cada uno asignarle un espacio en grid
+      que va de 1...5.
+
+      ahora, cuando este en view de un proyecto deberia poder moverme entre proyectos de formalibrew no solo los ultimos 5
+    */
+    const finalIndex = index + 1;
+    const isLarge = finalIndex % 2 === 0 ? 'large' : null;
+    const gridPosition = {
+      gridColumnStart: (finalIndex),
+      gridColumnEnd: (finalIndex + 1),
+      gridRowStart: (finalIndex),
+      gridRowEnd: (finalIndex + 1),
+    };
+    console.log(gridPosition);
     return {
-      isLarge: index % 2 ? 'large' : null,
+      isLarge,
+      gridPosition,
     };
   },
 };
