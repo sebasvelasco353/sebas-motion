ea3ed6a4a4b549f310d5f9b87ced85e7866bee23
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2021-02-26 23:47:56 -0600

    finished homepage in small screens

--- components/projectPreview.vue
+++ components/projectPreview.vue
@@ -2,7 +2,7 @@
 <nuxt-link :to="`${element.path}`" class="w-full mx-0 my-12"  exact>
   <div class="relative flex items-center justify-center projPreview__container">
     <img class="m-0 opacity-70 bg" :src="require(`@/assets/images/${element.image}`)" :alt="element.title" />
-    <p class="absolute z-30 text-3xl break-words w-60 ">{{ element.title }}</p>
+    <p class="absolute z-30 w-full text-3xl break-words text-center">{{ element.title }}</p>
   </div>
 </nuxt-link>
 </template>
@@ -38,36 +38,8 @@ export default {
 </script>
 
 <style scoped>
-/*a {
-  width: 100%;
-  max-height: 28rem;
-}
-.projPreview__container {
-  display: flex;
-  width: 100%;
-  height: 100%;
-  flex: 1;
-  border-radius: 30px;
-  justify-content: center;
-  align-items: center;
-  position: relative;
-  overflow: hidden;
-}
-.large {
-  height: 60.7rem;
-}
-.bg {
-  width:100%;
-  height: 100%;
-  position: absolute ;
-  filter: brightness(60%);
-}
 p {
   font-family: 'Lato';
-  font-weight: 500;
-  position: absolute;
-  font-size: 2rem;
-  color: #FFFFFF;
+  @apply text-white;
 }
- */
 </style>
