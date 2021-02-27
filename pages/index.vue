0ff3c7e994ce89db488088e922a5d22c9800b98d
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2021-02-26 22:58:42 -0600

    container is done

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="reel">
+    <div class="w-full reel">
       <!-- TODO: poner auto-play cuando termine pq me estoy enloqueciendo con la
       musica cada segundo -->
       <video height="100%" width="100%" controls>
@@ -35,32 +35,5 @@ export default {
 };
 </script>
 
-<style>
-.reel {
-  width: 75vw;
-  height: 80vh;
-  border-radius: 30px;
-  overflow: hidden;
-}
-img {
- margin: 0 auto;
-}
-/*h3 {*/
-  /*margin: 5rem auto;*/
-  /*font-size: 3.6rem;*/
-  /*width: 50%;*/
-/*}*/
-.projects {
-  margin: 15rem 0;
-  width: 100%;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-template-rows: 1fr 1fr 1fr;
-  column-gap: 5rem;
-  row-gap: 5rem;
-  grid-template-areas:
-    "a 1"
-    "2 1"
-    "2 3";
- }
+<style scoped>
 </style>
