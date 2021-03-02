68c6e2481266245eddafde62aac982f27938f742
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2021-03-01 21:07:49 -0600

    finished for today, need project images and navbar needs to be fixed top

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div class="w-full reel">
       <!-- TODO: poner auto-play cuando termine pq me estoy enloqueciendo con la
       musica cada segundo -->
-      <video height="100%" width="100%" class="rounded-2xl" controls>
+      <video height="100%" width="100%" class="rounded-2xl" controls autoplay>
         <source src="@/assets/video/Reel_2021.mp4" type="video/mp4">
         Tu navegador no tiene soporte para videos.
       </video>
@@ -14,9 +14,6 @@
     <img src="@/assets/images/separator.svg" alt="Separator images" class="mx-auto my-0">
     <div class="flex flex-col my-6 projects md:grid md:grid-cols-2 md:grid-rows-4 md:gap-6">
       <project-preview v-for="(project, index) in projects" :element="project" :key="index" :index="index"  />
-      <!-- <project-preview :element="projects[1]" class="col-span-1 row-span-2" :index="2"  />
-      <project-preview :element="projects[2]" class="col-span-1 row-span-1" :index="3"  />
-      <project-preview :element="projects[3]" class="col-span-1 row-span-2" :index="4"  /> -->
     </div>
    </div>
 </template>
@@ -31,7 +28,6 @@ export default {
   async asyncData({ $content }) {
     const projects = await $content('projects', { deep: true }).only(['title', 'image']).sortBy('fecha', 'desc')
       .fetch();
-    console.log(projects);
     return {
       projects,
     };
