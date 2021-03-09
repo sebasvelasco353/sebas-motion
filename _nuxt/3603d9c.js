d9206dca4d59a7181af746f02d773bec42610633
sebasvelasco353 <sebasvelasco353@users.noreply.github.com>
2021-03-09 02:51:19 +0000

    deploy: d3b1b3253419875624dc9e92ec12057d23e79e8c

---
+++ _nuxt/3603d9c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{281:function(e,n,t){"use strict";t.r(n);var l={},r=t(14),component=Object(r.a)(l,(function(){var e=this.$createElement;return(this._self._c||e)("p",[this._v("hello fren, dis is work")])}),[],!1,null,null,null);n.default=component.exports}}]);
