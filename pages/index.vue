1d31dc7c1de5f2fe7d9770ea3f7ea0943a49925d
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2021-03-19 23:23:17 -0600

    got distracted watching a cooking show and forgot to add menu animation, need to add this commit because im sleepy and might break something soon

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="home">
     <div class="w-full reel">
-      <!-- TODO: poner auto-play cuando termine pq me estoy enloqueciendo con la
-      musica cada segundo -->
-      <video height="100%" width="100%" class="rounded-2xl" controls >
+      <video height="100%" width="100%" class="rounded-2xl" controls autoplay>
         <source src="@/assets/video/Reel_2021.mp4" type="video/mp4">
         Tu navegador no tiene soporte para videos.
       </video>
