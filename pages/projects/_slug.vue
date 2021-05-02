c5fe932d0a8d0084229653570ef24ec676621de4
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2021-05-02 01:36:10 -0500

    wip

--- pages/projects/_slug.vue
+++ pages/projects/_slug.vue
@@ -1,8 +1,15 @@
 <template>
-  <div>
-    Hello
-    <img :src="project.thumbnail" alt="">
-  </div>
+  <article>
+    <h1>{{ project.title }}</h1>
+    <div class="row">
+      <nuxt-content :document="project" />
+      <img :src="require(`~/assets/images/projects/${getFileName(project.thumbnail)}`)" alt="">
+    </div>
+    <div class="row">
+      <img :src="require(`~/assets/images/projects/${getFileName(project.thumbnail2)}`)" alt="">
+      <img :src="require(`~/assets/images/projects/${getFileName(project.thumbnail3)}`)" alt="">
+    </div>
+  </article>
 </template>
 
 <script>
@@ -10,6 +17,14 @@ export default {
   data() {
     return {
     };
+  },
+  methods: {
+    getFileName(route) {
+      /* return route.split("/"); */
+      const fileName = route.split("/");
+      console.log(fileName[fileName.length-1]);
+      return fileName[fileName.length-1];
+    }
   },
   async asyncData({ $content, app, params }) {
     const project = await $content(`projects/`, params.slug).fetch();
@@ -19,4 +34,17 @@ export default {
 </script>
 
 <style scoped>
+p {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+}
+.row {
+  display: flex;
+  flex-direction: row;
+}
+.row img {
+  width: 48%;
+}
 </style>
